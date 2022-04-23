726d991c8573e1ecc256db9ecb2dca4bf0bfaf48
Assis Ferreira <ferreira.assisruny@gmail.com>
2022-04-23 15:33:56 -0100

    responsive layout class

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,13 +10,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+
+    final currentWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         title: const Center(child: Text("Hello World")),
       ),
       body: Container(
-        child: Column(children: [Text("Hello")]),
+        child: Column(children: [Text(currentWidth.toString())]),
       ),
     );
   }
